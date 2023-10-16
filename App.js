7e3a0488d643e28afe774488bae088124f0a3fb0
saicharan <saicharang@monocept.com>
2023-10-16 16:03:58 +0530

    OTA sample

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>MLI Super App OTA Sample</Text>
+      <Text>version: 1.0.3</Text>
+      <TouchableOpacity
+      onPress={()=>alert("clicked")}
+      >
+        <Text>
+          Click here
+        </Text>
+        </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
